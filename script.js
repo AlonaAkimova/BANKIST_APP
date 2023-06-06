be63ddc61cec4cb5adf8b0e6d2e0f3a37f775fab
AlonaAkimova <aliona.akimova@gmail.com>
2023-06-06 16:48:08 +0700

    changes

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -28,4 +29,79 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(document.getElementsByClassName('btn'));
+
+//Creating and Inserting elements
+
+//.insertAdjacentHTML
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use cookies for improved functionality and analytics';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics <button class ="btn btn--close-cookie"> Got it! </button>';
+// // header.prepend(message);
+// header.append(message);
+
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// DELETE MESSAGE
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     // message.remove();
+//     message.parentElement.removeChild(message);
+//   });
+
+// //Styles
+
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// console.log(getComputedStyle(message).color);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height + 10) + 40 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// //Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.src);
+// logo.alt = 'Beautiful minimalist logo';
+// logo.setAttribute('company', 'Bankist');
+// console.log(logo.getAttribute('src'));
+
+// const link = document.querySelector('.twitter-link');
+// console.log(link.href);
+
+// //Data attributes
+// console.log(logo.dataset.versionNumber);
+// //Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+// logo.className = 'alona'; //dont use!! overwrite classes
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(e.target.getBoundingClientRect());
+  console.log('height/width', document.documentElement.clientHeight);
+  //scrolling
+  window.scrollTo(s1coords.left, s1coords.top);
 });
